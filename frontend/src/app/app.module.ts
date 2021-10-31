18d8feea3107d178001a95c63643bac5306dbdb0
fuhr-br <andersonfuhr@yahoo.com.br>
2021-10-31 14:49:38 -0300

    add component procuct-create

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './views/home/home.component';
 import {MatCardModule}from '@angular/material/card';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
 import { RedDirective } from './directives/red.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +24,8 @@ import { RedDirective } from './directives/red.directive';
     NavComponent,
     HomeComponent,
     ProductCrudComponent,
-    RedDirective
+    RedDirective,
+    ProductCreateComponent
   ],
   imports: [
     BrowserModule,
