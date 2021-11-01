f72866e1899eb1873acdb2f5526e29f2ae0a3281
fuhr-br <andersonfuhr@yahoo.com.br>
2021-11-01 19:58:19 -0300

    remove bug router

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,15 +1,28 @@
+import { Router } from '@angular/router';
+import { ProductService } from './../product.service';
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../product.model';
 @Component({
   selector: 'app-product-update',
   templateUrl: './product-update.component.html',
   styleUrls: ['./product-update.component.css']
 })
 export class ProductUpdateComponent implements OnInit {
+    product : Product;
+  constructor(private productService : ProductService, 
+    private router : Router) { 
+    this.product = { }
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
 
+  updateProduct(): void{
+
+  }
+  
+  cancel(): void{
+    this.router.navigate(['/products'])
+  }
 }
